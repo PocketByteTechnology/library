2895a04f2ab8ecc32834197beb6a4cde7658384c
omrawaley <omrawaley@icloud.com>
2026-08-06 11:07:49 -0400

    feat(gfx_pax): add double buffering to eliminate screen tearing and artifacts

--- gfx_pax.c
+++ gfx_pax.c
@@ -9,23 +9,33 @@ extern esp_lcd_panel_handle_t pb_panel_handle;
 
 #define TAG "pb_gfx_pax"
 
+#define NUM_BUFFERS 2
+
+static pax_buf_t gfx_bufs[NUM_BUFFERS];
+static volatile uint8_t draw_idx;
+
 static pax_buf_t gfx_buf;
 
 void pb_gfx_pax_init(void)
 {
     pb_gfx_init();
-    pax_buf_init(&gfx_buf, NULL, LCD_HEIGHT, LCD_WIDTH, PAX_BUF_16_565RGB);
-    pax_buf_reversed(&gfx_buf, true);
-    pax_buf_set_orientation(&gfx_buf, PAX_O_ROT_CCW);
+    for (int i = 0; i < NUM_BUFFERS; ++i) {
+        pax_buf_init(&gfx_bufs[i], NULL, LCD_HEIGHT, LCD_WIDTH, PAX_BUF_16_565RGB);
+        pax_buf_reversed(&gfx_bufs[i], true);
+        pax_buf_set_orientation(&gfx_bufs[i], PAX_O_ROT_CCW);
+    }
+    draw_idx = 0;
 }
 
 void pb_gfx_pax_flush(void)
 {
     esp_lcd_panel_draw_bitmap(pb_panel_handle, 0, 0, LCD_HEIGHT, LCD_WIDTH,
-        pax_buf_get_pixels(&gfx_buf));
+        pax_buf_get_pixels(&gfx_bufs[draw_idx]));
+
+    draw_idx = (uint8_t)((draw_idx + 1) % NUM_BUFFERS);
 }
 
 pax_buf_t *pb_gfx_pax_get_buf(void)
 {
-    return &gfx_buf;
+    return &gfx_bufs[draw_idx];
 }
